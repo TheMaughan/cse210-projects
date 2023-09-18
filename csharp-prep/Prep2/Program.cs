36778fe2a94d1f776e5b1d7ee9391a5128e9b86f
Bryce <mau16009@byui.edu>
2023-09-17 21:20:53 -0600

    Started Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        /*"int A = 90;
+        int B = 80;
+        int C = 70;
+        int D = 60;*/
+        //int F = 60;
+
+        //Console.WriteLine($"{A} {B} {C} {D}");
+
+        Console.Write("What is your grade percentage? ");
+        string yourGrade = Console.ReadLine();
+        int percent = int.Parse(yourGrade);
+
+        if (percent >= 90)
+        {
+            Console.WriteLine("Your letter grade is A.");
+        }
+        else if (percent >= 80)
+        {
+            Console.WriteLine("Your letter grade is B.");
+        }
+        else if (percent >= 70)
+        {
+            Console.WriteLine("Your letter grade is C.");
+        }
+        else if (percent >= 60)
+        {
+            Console.WriteLine("Your letter grade is D.");
+        }
+        else
+        {
+            Console.WriteLine("Your letter grade is F.");
+        }
+
+
+        if (percent > 70)
+        {
+            Console.WriteLine("You passed! (⌐■_■)");
+        }
+        else
+        {
+            Console.WriteLine("You failed... (╯°□°）╯︵ ┻━┻");
+        }
+
+
+        Console.WriteLine("");
+        Console.WriteLine($"Your letter grade is {yourGrade}.");
     }
 }
