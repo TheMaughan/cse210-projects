aa1c0aa406ab32454ef27ca417b6df26d1a6d3a2
Bryce <mau16009@byui.edu>
2023-09-18 10:01:29 -0600

    Prep3 Complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+        Console.WriteLine($"Number is {number}");
+        /*for (int i = 2; i <= 20; i = i + 2)
+        {
+            Console.WriteLine(i);
+        }*/
+
+        Console.WriteLine("The magic number is bettween 1 & 10.");
+
+        int guess = -1;
+
+
+        while (guess != number)
+        {
+            Console.Write("What is the magic number? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < number)
+            {
+                Console.WriteLine("Guess Higher (⌐_⌐ )");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Guess Lower ( ¬_¬)");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it! (⌐■_■)");
+            }
+        }
     }
 }
