c94b0780962ebce183162da4f507c03e31e3704a
Bryce <mau16009@byui.edu>
2023-09-18 15:20:52 -0600

    Prep4 Started

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numList = new List<int>();
+        int number = -1;
+
+        
+        
+        Console.WriteLine("");
+        Console.WriteLine("Enter a list of numbers, type \"0\" when you're finished.");
+        
+        while (number != 0)
+        {         
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());    
+
+            //Console.WriteLine($"The number list is {number}");
+            if (number != 0)
+            {
+                numList.Add(number);
+            }
+        }
+
+
+        Console.WriteLine("");
     }
 }
