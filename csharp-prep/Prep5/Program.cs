bb97bebfe47d3c4ae8a686478487f119217f8969
Bryce <mau16009@byui.edu>
2023-09-19 22:55:44 -0600

    Prep5 Complete (⌐■_■)*

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Hello();
+
+        string name = YourName();
+        Console.WriteLine("");
+        int number = FavNum();
+        int squareNum  = SquareNum(number);
+        Display(name, squareNum);
+        Console.WriteLine("");
+        
+    }
+    static void Hello()
+    {
+        Console.WriteLine("");
+        Console.WriteLine("( ^_^)`/` Welcome to the Program!");
+        Console.WriteLine("");
+    }
+    static string YourName()
+    {
+        Console.Write("( =_=)? What is your name? ");
+        string userName = Console.ReadLine();
+
+        Console.WriteLine("");
+        Console.WriteLine($"( ^o^)`/` Hello {userName}!");
+
+        return userName;
+    }
+    static int FavNum()
+    {
+        Console.Write("*(¬_¬ )9 ~ Sooo... What's your favorite number? ");
+        string userNum = Console.ReadLine();
+        int num = int.Parse(userNum);
+
+        return num;
+    }
+    
+    static int SquareNum(int num)
+    {
+        int square = num * num;
+
+        return square;
+    }
+    static void Display(string name, int square)
+    {
+        Console.WriteLine("");
+        Console.WriteLine($"(⌐■_■)* {name}, the square of your favorite number is {square}!");
+        Console.WriteLine("");
+        Console.WriteLine("( T_T)`/` Good bye!");
     }
 }
