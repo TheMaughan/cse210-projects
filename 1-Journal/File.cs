79cc1a1a1fc6eeaed2ef2d90cf2d02af13e571b8
Bryce <mau16009@byui.edu>
2023-10-04 09:12:52 -0600

    Started project

---
+++ 1-Journal/File.cs
@@ -0,0 +1,7 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public class File
+{
+	
+}
---
+++ 1-Journal/Program.cs
@@ -0,0 +1,4 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+// See https://aka.ms/new-console-template for more information
+File file = new File
