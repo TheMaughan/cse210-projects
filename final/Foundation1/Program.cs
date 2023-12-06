217daeb38b5b653899f9ddf9ee02eff451505c56
Bryce <mau16009@byui.edu>
2023-12-06 03:39:13 -0700

    Kicking off the Final Projects!

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Foundation1 World!");
+
+        // VideoManager videoManager = new VideoManager();
+        // videoManager.LoadAndDisplayVideos("videoData.yaml");
     }
 }
